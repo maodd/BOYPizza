db5afd6ff2748ec6f68979f6b73c0ffa925ebb57
Frank Mao <maodd.ca@gmail.com>
2016-12-09 14:58:04 -0700

    added icons and pods.

---
+++ Pods/Target Support Files/Pods-Build Your Own pizzas/Pods-Build Your Own pizzas-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Build_Your_Own_pizzas : NSObject
+@end
+@implementation PodsDummy_Pods_Build_Your_Own_pizzas
+@end
---
+++ Pods/Target Support Files/Pods-Build Your Own pizzas/Pods-Build Your Own pizzas-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Build_Your_Own_pizzasVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Build_Your_Own_pizzasVersionString[];
+
