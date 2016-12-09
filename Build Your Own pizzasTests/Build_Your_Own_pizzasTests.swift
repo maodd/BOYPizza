21ad35909880409bfeaa6eba6100c4db7d8f17dc
Frank Mao <maodd.ca@gmail.com>
2016-12-09 08:14:19 -0700

    Initial Commit

---
+++ Build Your Own pizzasTests/Build_Your_Own_pizzasTests.swift
@@ -0,0 +1,36 @@
+//
+//  Build_Your_Own_pizzasTests.swift
+//  Build Your Own pizzasTests
+//
+//  Created by Frank Mao on 2016-12-09.
+//  Copyright © 2016 mazoic. All rights reserved.
+//
+
+import XCTest
+@testable import Build_Your_Own_pizzas
+
+class Build_Your_Own_pizzasTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
